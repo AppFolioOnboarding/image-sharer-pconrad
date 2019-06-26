b16e10978c81bd084637a01c110e9a3c0352e2b6
Phill Conrad <phillip.conrad@appfolio.com>
2019-06-26 09:53:20 -0700

    pc/ns - refactor seeds to use create!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup,.
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }],
+#   Character.create(name: 'Luke' }, movie: movies.first,
 
-Image.create(url: 'https://learn.appfolio.com/apm/assets/jason-randall-220x330.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/klaus-schauser-220x330.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/jon-walker-220x330.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/ida-kane-220x330.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/kim-coalson-220x330.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/aimee-miller-220x330.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/amy-meyer-220x330.jpg')
-Image.create(url: 'http://learn.appfolio.com/apm/assets/01-timeline-large-screen-1500x465.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/assets/work-with-us-large-1500x434.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/Cambridge_CS_tile_340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/VistaPM_Shinobu_CS_tile_340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/4-degress-case-study-filter-340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/long_foster-case-study-filter-340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/db-chamberlin-case-study-filter-340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/sundance-llc-case-study-filter-340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/case-study/yale-filter-340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/images/casestudies/gsf-case-study-filter-img-340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/images/casestudies/libertymanagement_340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/images/casestudies/coldwell_banker_340x190.jpg')
-Image.create(url: 'https://learn.appfolio.com/apm/images/casestudies/carina-lyons-filter-img-340x190.jpg')
+Image.create!([
+  { url: 'https://learn.appfolio.com/apm/assets/jason-randall-220x330.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/klaus-schauser-220x330.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/jon-walker-220x330.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/ida-kane-220x330.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/kim-coalson-220x330.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/aimee-miller-220x330.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/amy-meyer-220x330.jpg' },
+  { url: 'http://learn.appfolio.com/apm/assets/01-timeline-large-screen-1500x465.jpg' },
+  { url: 'https://learn.appfolio.com/apm/assets/work-with-us-large-1500x434.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/Cambridge_CS_tile_340x190.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/VistaPM_Shinobu_CS_tile_340x190.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/4-degress-case-study-filter-340x190.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/long_foster-case-study-filter-340x190.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/db-chamberlin-case-study-filter-340x190.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/sundance-llc-case-study-filter-340x190.jpg' },
+  { url: 'https://learn.appfolio.com/case-study/yale-filter-340x190.jpg' },
+  { url: 'https://learn.appfolio.com/apm/images/casestudies/gsf-case-study-filter-img-340x190.jpg' },
+  { url: 'https://learn.appfolio.com/apm/images/casestudies/libertymanagement_340x190.jpg' },
+  { url: 'https://learn.appfolio.com/apm/images/casestudies/coldwell_banker_340x190.jpg' },
+  { url: 'https://learn.appfolio.com/apm/images/casestudies/carina-lyons-filter-img-340x190.jpg' }
+])
